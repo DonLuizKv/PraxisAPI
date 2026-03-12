801f05b710459c5e90a2c5d6a0efb6022d2efe4c
DonLuizKv <luizkv4@outlook.com>
2026-03-12 00:13:46 -0500

    Actualizado el middleware de errores

--- src/middlewares/errorHandler.middleware.ts
+++ src/middlewares/errorHandler.middleware.ts
@@ -1,11 +1,11 @@
 import { ErrorRequestHandler, NextFunction, Request, Response } from "express";
 import { ErrorManager } from "../lib/ErrorManager";
-import { Logger } from "../lib/Logger";
 
 export const errorHandler: ErrorRequestHandler = (err: ErrorManager, req: Request, res: Response, next: NextFunction) => {
     const { statusCode, status, message } = err;
 
     res.status(statusCode).json({
+        statusCode,
         status,
         message: message || "Internal server error",
     });
