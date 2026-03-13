d13283f2f7d2adde5aef4dd559cc5a8c3cd6921c
DonLuizKv <luizkv4@outlook.com>
2026-03-12 22:46:28 -0500

    actualizadas funciones comunes

--- src/utilities/utils.ts
+++ src/utilities/utils.ts
@@ -1,17 +1,4 @@
 import { randomBytes } from "node:crypto";
-import { Logger } from "../lib/Logger";
-
-
-export const validateEnvironmentVariables = (variables: string[]): void => {
-    const missing = variables.filter((env) => !process.env[env]);
-
-    if (missing.length > 0) {
-        Logger.error(`Missing required environment variables: ${missing.join(", ")}`, {
-            prefix: "\n"
-        });
-        process.exit(1);
-    }
-}
 
 export const generateUID = (prefix: string, length = 6): string => {
     const CHARS: string = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
