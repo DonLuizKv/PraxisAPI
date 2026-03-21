a3f94c20c9539a6abeb0dea348d8074ebfa01593
DonLuizKv <luizkv4@outlook.com>
2026-03-20 22:50:47 -0500

    updated token repository

--- src/modules/Auth/token.repository.ts
+++ src/modules/Auth/token.repository.ts
@@ -2,9 +2,10 @@ import { Database } from "../../infra/database/Database";
 import { Repository } from "../../infra/database/Repository";
 import { TableNames } from "../../infra/database/Tables";
 
+// como se ve en la database
 interface TokenEntity {
     id: number;
-    sub: number; // ID del usuario
+    userID: number;
     token: string;
     expires_at: Date;
 }
@@ -14,12 +15,12 @@ export class TokenRepository extends Repository<TokenEntity> {
         super(TableNames.Tokens, db);
     }
 
-    async CreateToken(token: string, sub: number, expires_at: Date): Promise<void> {
-        await this.Create({ token, sub, expires_at });
+    async CreateToken(token: string, userID: number, expires_at: Date): Promise<void> {
+        await this.Create({ token, userID, expires_at });
     }
 
     async FindTokenByUserID(userID: number): Promise<TokenEntity | null> {
-        return this.Find("sub", userID);
+        return this.Find("userID", userID);
     }
 
     async FindTokenByToken(token: string): Promise<TokenEntity | null> {
